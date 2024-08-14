82c57625a6730ff0318a55d327564698b32ccc28
amit690 <amitsanyal191@gmail.com>
2024-08-14 12:48:24 +0530

    body-pass

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const bodyParser=require("body-parser")
 
 const app = express(); 
 
-app.use((req, res, next) => {
-    console.log('I am in middleware');
-    next(); // Pass control to the next middleware function
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/favicon.ico',(req, res, next) => {
+    // Ignore favicon requests
+    res.status(204).end(); // No Content
+});
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Submit</button></form>');
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use((req, res, next) => {
     console.log('Another middleware');
     res.send('<h1>I am so excited to learn Express</h1>');
 });
+
 
 app.listen(3000);
