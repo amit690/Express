cc7baa7f6c8bc7890bb5111d1c0a13986a90ccfa
amit690 <amitsanyal191@gmail.com>
2024-08-15 12:19:44 +0530

    adding Routers and Filters

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const bodyParser=require("body-parser")
 
 const app = express(); 
 
+const adminRouters=require('./routes/admin.js');
+
+const shopRouters=require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/favicon.ico',(req, res, next) => {
@@ -10,19 +14,14 @@ app.use('/favicon.ico',(req, res, next) => {
     res.status(204).end(); // No Content
 });
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Submit</button></form>');
+app.use('/admin',adminRouters);
+app.use('/shop',shopRouters);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>404 page not found</h1>');
 });
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
 
-app.use((req, res, next) => {
-    console.log('Another middleware');
-    res.send('<h1>I am so excited to learn Express</h1>');
-});
 
 
 app.listen(3000);
